9f44aaf99b4e7bbc0562e232d5f76c98d5d6d390
Michael Mkwelele <memkwelele@gmail.com>
2020-11-11 10:45:24 -0800

    Initialise repository.

---
+++ src/main/java/com/mtotowamkwe/lostboyz/LostboyzApplication.java
@@ -0,0 +1,13 @@
+package com.mtotowamkwe.lostboyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LostboyzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LostboyzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mtotowamkwe/lostboyz/LostboyzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mtotowamkwe.lostboyz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LostboyzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
