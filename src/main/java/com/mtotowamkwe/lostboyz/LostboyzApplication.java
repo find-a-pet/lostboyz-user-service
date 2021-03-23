bb309223c42cf4c302a60b1708dc4a753fc36e6e
Michael Mkwelele <15795163+mtotoWamkwe@users.noreply.github.com>
2021-03-23 14:17:16 -0700

    Set the system property java.runtime.version to Java 11.

--- src/main/java/com/mtotowamkwe/lostboyz/LostboyzApplication.java
+++ src/main/java/com/mtotowamkwe/lostboyz/LostboyzApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LostboyzApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("java.runtime.version", "11");
 		SpringApplication.run(LostboyzApplication.class, args);
 	}
 
